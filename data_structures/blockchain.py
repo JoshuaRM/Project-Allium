9bb31e5832288786878824c05a4c2aa097f742ca
Joshua Mayers <33873582+JoshuaRM@users.noreply.github.com>
2018-11-16 07:55:38 -0500

    Added add_block method

--- data_structures/blockchain.py
+++ data_structures/blockchain.py
@@ -1,11 +1,15 @@
 import os.path
+from struct import pack
+
+magic_bytes = pack('L', 3652501241)
+
+
 class Blockchain:
     
     def __init__(self,filename):
         """
         Constructor that takes in a blockchain to create a copy of it
         in the class data member blockfile
-
         :param filename: local copy of the blockchain that will
                         be used to create this copy of the blockchain
         :no return:
@@ -14,3 +18,26 @@ class Blockchain:
         # If the file does not already exist, create the file and close it
         if not (os.path.isfile(filename)):
             with open(filename, 'wb') as f: pass
+        
+    def add_block(self, block):
+        size = get_size_bytes(self, block)
+        payload = magic_bytes+size+block
+        
+        with open(self.blockfile, 'ab') as fileobj:
+            fileobj.write(payload)
+
+    
+def get_size_bytes(self, byteString):
+    return pack('I', len(byteString))
+
+def read_from_file(filename: str):
+    payload=[]
+    with open(filename, 'rb') as file:
+        while True:
+            char = file.read(1)
+            if not char:
+                break
+            payload.append(char)
+    
+    return payload
+
--- data_structures/blockchain.test.py
+++ data_structures/blockchain.test.py
@@ -1,28 +1,57 @@
 # imports
 import unittest
 import os
-from blockchain import *
+from blockchain import *    
+import block as B
 
+
+    
 class TestBlock(unittest.TestCase):
-	def setUp(self):
-		self.bc = Blockchain("testfile")
-
-	def tearDown(self):
-		os.remove("testfile")
-
-	def test_constructor(self):
-		filename = "testfile"
-		self.assertEqual(self.bc.blockfile,filename)
-		self.assertTrue(os.path.isfile(filename))
-		# Opens the blockchain file and writes '0' to it
-		with open(self.bc.blockfile, 'wb') as fileobj:
-		    fileobj.write(b'0')
-		# Creates another blockchain with the same filename as test_blockchain
-		test_blockchain_2 = Blockchain(self.bc.blockfile)
-		# Ensures that the file of test_blockchain is not overwritten
-		with open(self.bc.blockfile, 'rb') as fileobj:
-		    char = fileobj.read(1)
-		self.assertEqual(b'0', char)
-
-if __name__ == '__main__':
-    unittest.main()
+    def setUp(self):
+        self.bc = Blockchain("testfile")
+        
+    def tearDown(self):
+        os.remove("testfile")
+    
+    
+    def test_constructor(self):
+        
+        filename = "testfile"
+        self.assertEqual(self.bc.blockfile, filename)
+        self.assertTrue(os.path.isfile(filename))
+        # Opens the blockchain file and writes '0' to it
+        with open(self.bc.blockfile, 'ab') as fileobj:
+            fileobj.write(b'0')
+        # Creates another blockchain with the same filename as test_blockchain
+        test_blockchain_2 = Blockchain(self.bc.blockfile)
+        # Ensures that the file of test_blockchain is not overwritten
+        with open(self.bc.blockfile, 'rb') as fileobj:
+            char = fileobj.read(1)
+        self.assertEqual(b'0', char)
+          
+    def test_size(self):
+        #Create a byte string to compare to
+        testString = b'testing'
+        #Create int byte of the length of byte string
+        testLength = B.int_to_bytes(len(testString))
+        #Check if int byte is equal
+        self.assertEqual(get_size_bytes(self, testString), testLength)
+        
+    def test_add_block(self):
+        target = 10**72     # Target for which all block hashes must be under
+        data = B.hash_SHA("Testing block".encode())   # valid block data to be used for block construction
+        prev_hash = B.hash_SHA("0".encode())
+        prev_hash2 = B.hash_SHA("1".encode())
+        
+        bc = B.mine(prev_hash, data, target)
+        bc2 = B.mine(prev_hash2, data, target)
+        size = get_size_bytes(self, bc)
+        self.bc.add_block(bc)
+        self.bc.add_block(bc2)
+        
+        
+        payload=read_from_file(self.bc.blockfile)
+        print(bc)
+        
+    if __name__ == '__main__':
+        unittest.main()
